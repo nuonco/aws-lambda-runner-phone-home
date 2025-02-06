f03d74b2deb9c838e29ea7362a0bddb606d712cb
sam boyer <sam@nuon.co>
2025-02-06 17:36:45 -0500

    chore: Switch back to python...again (#4)
    * chore: Switch back to python...again
    
    * Remove all go and container bits
    
    * Final updates to scripts

--- phonehome.py
+++ phonehome.py
@@ -5,33 +5,37 @@ import cfnresponse
 
 http = urllib3.PoolManager()
 
+
 def lambda_handler(event, context):
-	data = event["ResourceProperties"] | {
-        "deprovision_arn": data.get("DeprovisionRole", ""),
-        "provision_arn": data.get("ProvisionRole", ""),
-        "maintenance_arn": data.get("MaintenanceRole", ""),
-        "breakglass_arn": data.get("BreakGlassRole", ""),
-		"stack_id": event["StackId"],
-		"state": event["RequestType"],
-	}
+    props = event["ResourceProperties"]
+    data = {
+        "deprovision_arn": props.get("DeprovisionRole", ""),
+        "provision_arn": props.get("ProvisionRole", ""),
+        "maintenance_arn": props.get("MaintenanceRole", ""),
+        "breakglass_arn": props.get("BreakGlassRole", ""),
+        "runner_id": props.get("RunnerId", ""),
+        "stack_id": event["StackId"],
+        "state": event["RequestType"],
+    }
     # TODO(sdboyer) replace this whole-URL injection with a static base + an install id, once the API exists
     # url = "https://api.nuon.io/v1/installs/cloudformation-event/%s" % data["install_id"]
-    url = event["url"]
-	encoded_data = json.dumps(data).encode('utf-8')
 
-	try:
-		response = http.request(
-			"POST",
-			url,
-			body=encoded_data,
-			headers={"Content-Type": "application/json"},
-		)
-		print("Response: ", response.data)
-		cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
-	except Exception as e:
-		print("Error: ", str(e))
-		# It's OK if notifying Nuon fails on deletion
-		if event["RequestType"] in ["Create", "Update"]:
-			cfnresponse.send(event, context, cfnresponse.FAILED, {"Error": str(e)})
-		else:
-			cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
+    encoded_data = json.dumps(data).encode("utf-8")
+    url = props["url"]
+
+    try:
+        response = http.request(
+            "POST",
+            url,
+            body=encoded_data,
+            headers={"Content-Type": "application/json"},
+        )
+        print("Response: ", response.data)
+        cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
+    except Exception as e:
+        print("Error: ", str(e))
+        # It's OK if notifying Nuon fails on deletion
+        if event["RequestType"] in ["Create", "Update"]:
+            cfnresponse.send(event, context, cfnresponse.FAILED, {"Error": str(e)})
+        else:
+            cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
